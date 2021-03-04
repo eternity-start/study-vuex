537e10719f9808a190b5c0ec624eb0f2fcd4997e
谢小军 <7676606+xiexiaojunqq@user.noreply.gitee.com>
2021-03-04 19:58:40 +0800

    练习vuex

--- src/store/index.js
+++ src/store/index.js
@@ -5,11 +5,9 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    count: 1
   },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+  mutations: {},
+  actions: {},
+  modules: {}
 })
